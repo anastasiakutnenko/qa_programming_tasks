cd93d70a0688451d2778a828c79e0fcf0ee6a3ed
anastasiakutnenko <anastasia.kutnenko@gmail.com>
2018-01-16 14:20:42 +0200

    fix bug with different registers

--- src/task7/Task_7.java
+++ src/task7/Task_7.java
@@ -4,13 +4,14 @@ public class Task_7
 {
     public static void task_7()
     {
-      String str = "hananah";
+      String str = "HannaH";
+      String convertedToLow = str.toLowerCase();
       int n = str.length();
       int countElse = 0;
       for (int i = 0; i < n; i++)
       {
 
-          if (str.charAt(i) == str.charAt(n-1-i))
+          if (convertedToLow.charAt(i) == convertedToLow.charAt(n-1-i))
           {
           }
           else{
